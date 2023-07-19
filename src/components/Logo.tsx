8cf949053e63f45ac82942ce61e5c75e9bb5aae6
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2023-07-19 11:55:28 -0300

    fix: Prorise logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,16 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import ProriseLogo from "../assets/images/logo-priorise.svg";
+
 export const Logo = () => {
 	return (
 		<div className="w-fit">
 			<Link href="/">
-				<Image
-					src="https://lirp.cdn-website.com/0926e7b8/dms3rep/multi/opt/logo_final_site-c9a3aeea-114w.png"
-					alt="Office trends logo"
-					width={100}
-					height={40}
-				/>
+				<Image src={ProriseLogo} alt="Prorise logo" width={125} height={40} />
 			</Link>
 		</div>
 	);
