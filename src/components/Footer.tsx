1d7de742784d8fa90652e9db013c94dee6feda05
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2023-07-24 12:37:46 -0300

    feat: linked sections in footer | fix: element spacing in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,13 @@
 import { ContentContainer } from "@/components/ContentContainer";
+import { menuItems } from "@/constants/menuItems";
+import { scrollToSection } from "@/helpers/scroll";
 
 export const Footer = () => {
 	return (
 		<footer className="bg-dark-700 py-12">
 			<ContentContainer>
-				<div className="space-y-6 md:flex md:space-y-0">
-					<div className="flex-1 space-y-4">
+				<div className="justify-around space-y-6 md:flex md:space-y-0">
+					<div className="space-y-4">
 						<p className="text-4xl">Prorise</p>
 						<p className="max-w-xs text-base leading-7 text-custom-gray-500">
 							A good design is not only aesthetically pleasing, but also functional. It should be
@@ -13,29 +15,20 @@ export const Footer = () => {
 						</p>
 					</div>
 
-					<div className="flex-1">
+					<div>
 						<p className="mb-4 text-xl">Sections</p>
 
 						<div className="grid grid-cols-2 gap-x-4 gap-y-8 text-sm text-custom-gray-500 md:grid-cols-3">
 							<div className="flex flex-col space-y-3">
-								<a href="#">Home</a>
-								<a href="#">Section One</a>
-								<a href="#">Section Two</a>
-								<a href="#">Section Three</a>
-							</div>
-
-							<div className="flex flex-col space-y-3">
-								<a href="#">Home</a>
-								<a href="#">Section One</a>
-								<a href="#">Section Two</a>
-								<a href="#">Section Three</a>
-							</div>
-
-							<div className="flex flex-col space-y-3">
-								<a href="#">Home</a>
-								<a href="#">Section One</a>
-								<a href="#">Section Two</a>
-								<a href="#">Section Three</a>
+								{menuItems.map(({ name, sectionId }) => (
+									<a
+										key={name}
+										onClick={e => scrollToSection(e, sectionId)}
+										className="hover:cursor-pointer"
+									>
+										{name}
+									</a>
+								))}
 							</div>
 						</div>
 					</div>
