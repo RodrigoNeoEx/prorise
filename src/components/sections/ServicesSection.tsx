bb157d800f0e7b4c63fa30da6bd442bf4c0a201a
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2023-08-02 14:51:08 -0300

    feat: add icons on cards

--- src/components/sections/ServicesSection.tsx
+++ src/components/sections/ServicesSection.tsx
@@ -1,10 +1,13 @@
 import { ContentContainer } from "@/components/ContentContainer";
 import { SectionHeader } from "@/components/SectionHeader";
 
-import { GrUserManager, GrCode } from "react-icons/gr";
+import { GrUserManager } from "react-icons/gr";
 
 import { motion, useAnimationControls, useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
+import { MdMenuBook, MdOutlineFactory } from "react-icons/md";
+import { BsGraphUpArrow } from "react-icons/bs";
+import { FaPeopleArrows } from "react-icons/fa";
 
 export const ServicesSection = () => {
 	const servicesContainerRef = useRef(null);
@@ -21,31 +24,31 @@ export const ServicesSection = () => {
 
 	const services = [
 		{
-			icon: <GrCode size={40} color="white" />,
+			icon: <FaPeopleArrows size={40} color="black" />,
 			title: "Outsourcing",
 			description:
 				"Descubra a tranquilidade do Outsourcing de TI conosco. Nossa equipe de especialistas cuida da gestão e manutenção dos seus sistemas, permitindo que você foque no crescimento dos negócios. Simplifique, otimize e alcance resultados excepcionais.",
 		},
 		{
-			icon: <GrCode size={40} color="white" />,
+			icon: <BsGraphUpArrow size={40} color="black" />,
 			title: "Consultoria tech",
 			description:
 				"Como consultoria tech de TI oferecemos insights estratégicos e soluções personalizadas para impulsionar sua empresa rumo à excelência tecnológica. Aproveite nosso conhecimento especializado para alinhar a TI aos seus objetivos de negócios e alcançar resultados excepcionais.",
 		},
 		{
-			icon: <GrUserManager size={40} color="white" />,
+			icon: <GrUserManager size={40} color="black" />,
 			title: "Gestão e Administração",
 			description:
 				"Possuimos equipes especializadas em Gestão e Administração de TI para garantir operações eficientes e seguras. Deixe a infraestrutura tecnológica conosco e foque no sucesso dos seus negócios, com suporte confiável e soluções sob medida para suas necessidades.",
 		},
 		{
-			icon: <GrCode size={40} color="white" />,
+			icon: <MdOutlineFactory size={40} color="black" />,
 			title: "Software house",
 			description:
 				"Somos uma Software House de TI dedicada a criar soluções inovadoras e personalizadas para o seu negócio. Com expertise em desenvolvimento de software, entregamos produtos de alta qualidade, impulsionando a eficiência e o crescimento da sua empresa.",
 		},
 		{
-			icon: <GrCode size={40} color="white" />,
+			icon: <MdMenuBook size={40} color="black" />,
 			title: "Treinamentos",
 			description:
 				"Nossos treinamentos em tecnologia oferecem conhecimentos práticos e atualizados para capacitar sua equipe e alavancar sua performance no mundo digital. Desenvolva habilidades essenciais para enfrentar os desafios tecnológicos e impulsionar o sucesso do seu negócio.",
